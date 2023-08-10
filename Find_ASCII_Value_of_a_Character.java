d831d17e0f7ce949d6b5f322af4fd05a66a43634
sambasivagudala369 <119730311+sambasivagudala369@users.noreply.github.com>
2023-08-10 11:40:20 +0530

    import java.util.Scanner; public class asc{     public static void main(String[] args){         //String as;         Scanner sc=new Scanner(System.in);         char as=sc.next().charAt(0);         System.out.print((int)as);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class asc{
+    public static void main(String[] args){
+        //String as;
+        Scanner sc=new Scanner(System.in);
+        char as=sc.next().charAt(0);
+        System.out.print((int)as);
+    }
+}
