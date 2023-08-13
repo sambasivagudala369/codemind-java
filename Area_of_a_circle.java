f3522981d48a7281a8d7a2df2674d87e23cbd1de
sambasivagudala369 <119730311+sambasivagudala369@users.noreply.github.com>
2023-08-13 18:29:40 +0530

    import java.util.Scanner; public class cir{ public static void main(String[] args){     Scanner sc = new Scanner(System.in);     int r=sc.nextInt();     Double ar=3.14*r*r;     System.out.printf("%.2f",ar);
    }
    }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class cir{
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int r=sc.nextInt();
+    Double ar=3.14*r*r;
+    System.out.printf("%.2f",ar);
+    
+}
+}
