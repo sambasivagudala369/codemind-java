4bfe7acdf648bbaa7b77316cb243d3f903044e7b
sambasivagudala369 <119730311+sambasivagudala369@users.noreply.github.com>
2023-08-17 11:44:57 +0530

    import java.util.Scanner; public class temp{     public static void main(String[] args){         int t;         Scanner sc=new Scanner(System.in);         t=sc.nextInt();         if(t>20){             System.out.println("HOT");         }         else{             System.out.println("COLD");         }
        }
    }

---
+++ Climate.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class temp{
+    public static void main(String[] args){
+        int t;
+        Scanner sc=new Scanner(System.in);
+        t=sc.nextInt();
+        if(t>20){
+            System.out.println("HOT");
+        }
+        else{
+            System.out.println("COLD");
+        }
+        
+    }
+}
