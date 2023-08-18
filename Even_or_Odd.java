a0249029872653ec23fb5ff218838e9bf4942852
sambasivagudala369 <119730311+sambasivagudala369@users.noreply.github.com>
2023-08-18 07:33:33 +0530

    import java.util.Scanner; public class eo{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a;         a=sc.nextInt();         if(a%2==0){             System.out.println("Even");         }         else{             System.out.println("Odd");         }     } }

---
+++ Even_or_Odd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class eo{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        if(a%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
